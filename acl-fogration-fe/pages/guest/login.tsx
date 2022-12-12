b53322f1474f30a1f4f517c75ba77e90018f3e51
yahyaessam <yahya.elgaar@student.guc.edu.eg>
2022-12-12 15:35:02 +0100

    Added reviews in the course

--- acl-fogration-fe/pages/guest/login.tsx
+++ acl-fogration-fe/pages/guest/login.tsx
@@ -30,20 +30,39 @@ function Login() {
       <div className="flex items-center justify-center bg-[url('/images/bgacl.jpeg')] w-screen h-screen bg-cover bg-no-repeat">
         {/* div for the form */}
 
-        <div className="  flex flex-col items-center justify-start rounded-lg bg-white py-6 px-4">
-          <div className="flex flex-col items-start justify-start">
-            <InputField id="username" placeholder="Username" type="text" />
-            <InputField id="password" placeholder="Password" type="password" />
-            
+        <div className="  flex flex-col items-center justify-start rounded-lg bg-bc py-6 px-4">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <input
+              className="bg-black3 rounded-md p-3 w-72"
+              id="username"
+              placeholder="Username"
+              type="text"
+            />
+            <input
+              className="bg-black3 rounded-md p-3 w-72 "
+              id="password"
+              placeholder="Password"
+              type="password"
+            />
+
             {/* adding password input */}
             {/* <InputField id="password" placeholder="Password" type="password" /> */}
             {/* adding login button */}
             <Link href="/">
-              <button className="w-72 rounded-lg bg-lachmara p-2 text-white hover:bg-matisse hover:text-white">
+              <button className="w-72 rounded-lg bg-gradient-to-r to-babyblue from-purple p-2 text-white hover:bg-matisse hover:text-white">
                 Login
               </button>
             </Link>
-          
+            <div className="flex flex-col items-center justify-center underline text-violet-400 ">
+              <Link
+                href="/"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+              >
+                Forgot password?
+              </Link>
+            </div>
           </div>
         </div>
       </div>
