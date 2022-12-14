c4b90ef2d44b1a86935def793e4ea0a98e6fd8c6
Malak ElKhashab <malak.elkhashab@gmail.com>
2022-12-14 13:13:47 +0200

    Update CourseCard.tsx

--- acl-fogration-fe/components/molecules/CourseCard.tsx
+++ acl-fogration-fe/components/molecules/CourseCard.tsx
@@ -37,6 +37,21 @@ const CourseCard = () => {
     }
     return stars;
   };
+
+  
+  const discount =(discount:number,price:number) =>{
+      if(discount == 0){
+    return <div className="text-l">{price} $$</div>
+
+  }
+  else{ 
+    <div className="flex flex-row">
+    <div className="text-l line-through">{price} $$</div>
+     <div className="text-l">{(price) * (1-discount)} $$</div>
+     </div>
+  }
+  }
+ 
   return (
     <div className="m-2 grid grid-cols-1 place-items-center ">
       {courses.map((course) => (
@@ -46,12 +61,71 @@ const CourseCard = () => {
         >
           <div className=" font-bold text-2xl" key={course.id}>
             {course.name}
-            <div className="flex flex-row">{stars(course.rating)}</div>
+            <div className="flex flex-row">{stars(course.Course_Rating)}</div>
           </div>
 
-          <div className="text-l">{course.price} $$</div>
-          <div className="text-l"> {course.totalHours} Credit Hours</div>
+          
+          {/* <div className="text-l">{course.Course_Price} $$</div> */}
+          {discount(course.Course_Discount,course.Course_Price)}
+          <div className="text-l"> {course.Course_Hours} Credit Hours</div>
           <Button variant="contained">Book Course</Button>
+
+          {/* //Course content  */}
+          <div className="bg-black3 rounded-md m-6 flex flex-col p-2">
+            <h1 className="text-white font-bold text-3xl ">Course Content</h1>
+            {course.Course_Subtitle.map((subtitle) => {
+              return (
+                <div>
+                  <div className="flex flex-col gap-2 ">
+                    <div className="flex flex-row gap-2 justify-between">
+                      <div className="text-xl font-bold">
+                        {subtitle.Subtitle_Name}
+                      </div>
+                      <div className="text-l flex items-end ">
+                        Total Time: {subtitle.Subtitle_Hours} mins
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-2 w-[100%] ">
+                    {subtitle.Subtitle_Video.map((video) => {
+                      return (
+                        <div>
+                          <Link href={video.Video_Link}>
+                            <img
+                              className="flex-shrink-0"
+                              src="/images/pausedvideo.png"
+                              alt="No image yet 😅"
+                            />
+                          </Link>
+
+                          <div className="text-l">
+                            {video.Video_Description}
+                          </div>
+                          <div className="text-l">
+                            {video.Video_Length} mins
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+            <div className="bg-black3 rounded-md m-6 flex flex-col p-2 gap-1">
+              <div className="text-white font-bold text-l">Reviews</div>
+              <div className="flex flex-row gap-2">
+                {course.Course_Review.map((review,index) => (
+                  <div
+                    key={index}
+                    className="flex bg-gradient-to-l from-gray-700 to-black2 text-white p-6 rounded-md w-52"
+                  >
+                    {review}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
         </div>
       ))}
     </div>
