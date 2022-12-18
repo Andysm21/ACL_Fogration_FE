175d06b7ebbd2450e4beeeff5e7d92498bc22b79
hanapasha <hana.pasha@student.guc.edu.eg>
2022-12-18 01:40:45 +0100

    fixing maping errors

--- acl-fogration-fe/components/molecules/UserCourseCard.tsx
+++ acl-fogration-fe/components/molecules/UserCourseCard.tsx
@@ -6,131 +6,137 @@ import {AiFillFilePdf} from "react-icons/ai"
 import { TbCertificate } from "react-icons/tb";
 import { useState } from "react";
 const isCorporate = true;
-const courses = [
-  {
-    _id: {
-      $oid: "636820e12887948f062b493e",
-    },
-    Course_ID: 4,
-    Course_Title: "Data structures & Algorithms",
-    Course_Subject: "Computer Science",
-    Course_Description: "Data Structures and Algorithms",
-    Course_Price: 0,
-    Course_Rating: 5,
-    Course_Instructor: "Malak",
-    Course_Hours: 5,
-    Course_Country: "Egypt",
-    Course_Discount: 10,
-    Course_Discount_Duration: 5,
-    Course_Subtitle: [
-      {
-        Subtitle_ID: 1,
-        Subtitle_Name: "S1",
-        Subtitle_Course_ID: "1",
-        Subtitle_Video: [
-          {
-            Video_ID: 1,
-            Video_Link:
-              "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
-            Video_Subtitle: "S1",
-            Video_Description: "Introduction",
-            Video_Length: 10,
-          },
-        ],
-        Subtitle_Hours: "10",
-      },
-      {
-        Subtitle_ID: 2,
-        Subtitle_Name: "S2",
-        Subtitle_Course_ID: "1",
-        Subtitle_Video: [
-          {
-            Video_ID: 1,
-            Video_Link:
-              "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
-            Video_Subtitle: "S1",
-            Video_Description: "Introduction",
-            Video_Length: 10,
-          },
-        ],
-        Subtitle_Hours: "10",
-      },
-    ],
-    Course_Trainee: [{ Trainee_ID: 1, Trainee_Name: "Ahmed" }],
-    Course_Review: ["Very Good Course"],
-    Course_Rate: ["zeft"],
-    Course_Exam: [
-      { Exam_ID: "1", Exam_Question_ID: ["1", "2"], Exam_Grade: 50 },
-      { Exam_ID: "2", Exam_Question_ID: ["1", "2"], Exam_Grade: 90 },
-    ],
-  },
-  {
-    _id: {
-      $oid: "636820e12887948f062b493e",
-    },
-    Course_ID: 4,
-    Course_Title: "asda",
-    Course_Subject: "Computer Science",
-    Course_Description: "Data Structures and Algorithms",
-    Course_Price: 0,
-    Course_Rating: 5,
-    Course_Instructor: "Malak",
-    Course_Hours: 5,
-    Course_Country: "Egypt",
-    Course_Discount: 10,
-    Course_Discount_Duration: 5,
-    Course_Subtitle: [
-      {
-        Subtitle_ID: 1,
-        Subtitle_Name: "S1",
-        Subtitle_Course_ID: "1",
-        Subtitle_Video: [
-          {
-            Video_ID: 1,
-            Video_Link:
-              "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
-            Video_Subtitle: "S1",
-            Video_Description: "Introduction",
-            Video_Length: 10,
-          },
-        ],
-        Subtitle_Hours: "10",
-      },
-      {
-        Subtitle_ID: 2,
-        Subtitle_Name: "S2",
-        Subtitle_Course_ID: "1",
-        Subtitle_Video: [
-          {
-            Video_ID: 1,
-            Video_Link:
-              "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
-            Video_Subtitle: "S1",
-            Video_Description: "Introduction",
-            Video_Length: 10,
-          },
-        ],
-        Subtitle_Hours: "10",
-      },
-    ],
-    Course_Trainee: [{ Trainee_ID: 1, Trainee_Name: "Ahmed" }],
-    Course_Review: ["Very Good Course"],
-    Course_Rate: ["zeft"],
-    Course_Exam: [
-      { Exam_ID: "1", Exam_Question_ID: ["1", "2"], Exam_Grade: "A" },
-    ],
-    Course_What_You_Will_Learn: [
+// const courses = [
+//   {
+//     _id: {
+//       $oid: "636820e12887948f062b493e",
+//     },
+//     Course_ID: 4,
+//     Course_Title: "Data structures & Algorithms",
+//     Course_Subject: "Computer Science",
+//     Course_Description: "Data Structures and Algorithms",
+//     Course_Price: 0,
+//     Course_Rating: 5,
+//     Course_Instructor: "Malak",
+//     Course_Hours: 5,
+//     Course_Country: "Egypt",
+//     Course_Discount: 10,
+//     Course_Discount_Duration: 5,
+//     Course_Subtitle: [
+//       {
+//         Subtitle_ID: 1,
+//         Subtitle_Name: "S1",
+//         Subtitle_Course_ID: "1",
+//         Subtitle_Video: [
+//           {
+//             Video_ID: 1,
+//             Video_Link:
+//               "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
+//             Video_Subtitle: "S1",
+//             Video_Description: "Introduction",
+//             Video_Length: 10,
+//           },
+//         ],
+//         Subtitle_Hours: "10",
+//       },
+//       {
+//         Subtitle_ID: 2,
+//         Subtitle_Name: "S2",
+//         Subtitle_Course_ID: "1",
+//         Subtitle_Video: [
+//           {
+//             Video_ID: 1,
+//             Video_Link:
+//               "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
+//             Video_Subtitle: "S1",
+//             Video_Description: "Introduction",
+//             Video_Length: 10,
+//           },
+//         ],
+//         Subtitle_Hours: "10",
+//       },
+//     ],
+//     Course_Trainee: [{ Trainee_ID: 1, Trainee_Name: "Ahmed" }],
+//     Course_Review: ["Very Good Course"],
+//     Course_Rate: ["zeft"],
+//     Course_Exam: [
+//       { Exam_ID: "1", Exam_Question_ID: ["1", "2"], Exam_Grade: 50 },
+//       { Exam_ID: "2", Exam_Question_ID: ["1", "2"], Exam_Grade: 90 },
+//     ],
+//   },
+//   {
+//     _id: {
+//       $oid: "636820e12887948f062b493e",
+//     },
+//     Course_ID: 4,
+//     Course_Title: "asda",
+//     Course_Subject: "Computer Science",
+//     Course_Description: "Data Structures and Algorithms",
+//     Course_Price: 0,
+//     Course_Rating: 5,
+//     Course_Instructor: "Malak",
+//     Course_Hours: 5,
+//     Course_Country: "Egypt",
+//     Course_Discount: 10,
+//     Course_Discount_Duration: 5,
+//     Course_Subtitle: [
+//       {
+//         Subtitle_ID: 1,
+//         Subtitle_Name: "S1",
+//         Subtitle_Course_ID: "1",
+//         Subtitle_Video: [
+//           {
+//             Video_ID: 1,
+//             Video_Link:
+//               "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
+//             Video_Subtitle: "S1",
+//             Video_Description: "Introduction",
+//             Video_Length: 10,
+//           },
+//         ],
+//         Subtitle_Hours: "10",
+//       },
+//       {
+//         Subtitle_ID: 2,
+//         Subtitle_Name: "S2",
+//         Subtitle_Course_ID: "1",
+//         Subtitle_Video: [
+//           {
+//             Video_ID: 1,
+//             Video_Link:
+//               "https://www.youtube.com/watch?v=Q8TXgCzxEnw&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=2",
+//             Video_Subtitle: "S1",
+//             Video_Description: "Introduction",
+//             Video_Length: 10,
+//           },
+//         ],
+//         Subtitle_Hours: "10",
+//       },
+//     ],
+//     Course_Trainee: [{ Trainee_ID: 1, Trainee_Name: "Ahmed" }],
+//     Course_Review: ["Very Good Course"],
+//     Course_Rate: ["zeft"],
+//     Course_Exam: [
+//       { Exam_ID: "1", Exam_Question_ID: ["1", "2"], Exam_Grade: "A" },
+//     ],
+//     Course_What_You_Will_Learn: [
+//       "How to use React",
+//       "How to use Redux",
+//       "How to use Material UI",
+//     ],
+//   },
+// ];
+const  What_You_Will_Learn= [
       "How to use React",
       "How to use Redux",
       "How to use Material UI",
-    ],
-  },
-];
+    ];
 
 const UserCourseCard: React.FC<{ course }> = ({ course }) => {
-  if (courses.length === 0) {
-    return <div className="text-center "> No courses</div>;
-  }
+  
+  // if (courses.length === 0) {
+  //   return <div className="text-center "> No courses</div>;
+  // }
   const stars = (rating: number) => {
     let stars = [];
     for (let i = 0; i < rating; i++) {
@@ -140,10 +146,22 @@ const UserCourseCard: React.FC<{ course }> = ({ course }) => {
         </div>
       );
     }
+  
     return stars;
   };
     const [starsnum, setStarsnum] = useState(0);
+  const discount =(discount:number,price:number) =>{
+      if(discount == 0){
+    return <div className="text-l">{price} $$</div>
 
+  }
+  else{ 
+    <div className="flex flex-row">
+    <div className="text-l line-through">{price} $$</div>
+     <div className="text-l">{(price) * (1-discount)} $$</div>
+     </div>
+  }
+  }
   return (
     <div
       key={course.Course_ID}
@@ -162,7 +180,7 @@ const UserCourseCard: React.FC<{ course }> = ({ course }) => {
           <div className="bg-bc flex flex-col  gap-3 my-2">
             <div>{course.Course_Description}</div>
             <div className="flex flex-row">
-              {course.Course_Trainee.length} enrolled students, taught by{" "}
+              {course.Course_Users} enrolled students, taught by{" "}
               <div className="text-bc">.</div>
               <Link href="/[{course.Course_Instructor}]">
                 {/* // 23deli el link */}
@@ -190,9 +208,10 @@ const UserCourseCard: React.FC<{ course }> = ({ course }) => {
           </div>
           {/* //h1 el se3r */}
           <div className="flex flex-row justify-between my-2 ">
-            {!isCorporate && <h1 className=" text-violet-400 text-4xl font-bold ">
+            {/* {!isCorporate && <h1 className=" text-violet-400 text-4xl font-bold ">
               $${course.Course_Price}
-            </h1> }
+            </h1> } */}
+             {discount(course.Course_Discount,course.Course_Price)}
             
             <Link href="/">
               {/* //link button to enroll */}
@@ -213,12 +232,12 @@ const UserCourseCard: React.FC<{ course }> = ({ course }) => {
           <div className="text-white font-bold text-l">What you will learn</div>
           <div className="flex flex-col gap-1">
             {/* //m7taga 23melha grid */}
-            {course.Course_What_You_Will_Learn.map((item) => (
-              <div className="flex flex-row gap-1 text-white items-center">
+            {What_You_Will_Learn.map((item,index) => {return (
+              <div key={index} className="flex flex-row gap-1 text-white items-center">
                 <TiTick />
                 <div className="text-white">{item}</div>
               </div>
-            ))}
+            )})}
           </div>
         </div>
       </div>
@@ -250,9 +269,9 @@ const UserCourseCard: React.FC<{ course }> = ({ course }) => {
       {/* //Course content  */}
       <div className="bg-black3 rounded-md m-6 flex flex-col p-2">
         <h1 className="text-white font-bold text-3xl ">Course Content</h1>
-        {course.Course_Subtitle.map((subtitle) => {
+        {course.Course_Subtitle && course.Course_Subtitle.map((subtitle,index) => {
           return (
-            <div>
+            <div key={index} >
               <div className="flex flex-col gap-2 ">
                 <div className="flex flex-row gap-2 justify-between">
                   <div className="text-xl font-bold">
@@ -289,9 +308,9 @@ const UserCourseCard: React.FC<{ course }> = ({ course }) => {
           Course Material
         </div>
         <div className="flex flex-row p-2">
-            {course.Course_Exam.map((item) => {
+            {course.Course_Exam && course.Course_Exam.map((item,index) => {
               return(
-              <div key={course.Course_Exam.Exam_ID}  className="flex flex-col items-start ">
+              <div key={index}  className="flex flex-col items-start ">
                       <AiFillFilePdf size={100}/>
                       <div className="items-center justify-center flex flex-col">
                         <div className="text-l">Exam {item.Exam_ID}</div>
@@ -307,8 +326,8 @@ const UserCourseCard: React.FC<{ course }> = ({ course }) => {
           <div className="text-white font-bold text-l">Reviews</div>
 
           <div className="flex flex-row gap-2">
-            {course.Course_Review.map((review) => (
-                <div key={course.Course_ID} className="flex bg-gradient-to-l from-gray-700 to-black2 text-white p-6 rounded-md w-52">
+            {course.Course_Review && course.Course_Review.map((review,index) => (
+                <div key={index} className="flex bg-gradient-to-l from-gray-700 to-black2 text-white p-6 rounded-md w-52">
                   {review}</div>    
             ))}
           </div>
