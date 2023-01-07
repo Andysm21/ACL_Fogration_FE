f87fcdbd67657881f7454f55670fcd46bc2b8231
salmasleem <salmasleem@outlook.com>
2023-01-07 16:53:58 +0100

    Rate Instructor

--- acl-fogration-fe/components/molecules/UserCourseCard.tsx
+++ acl-fogration-fe/components/molecules/UserCourseCard.tsx
@@ -150,6 +150,7 @@ const enroll = (isCorporate:string) => {
             <div className="flex flex-row">
               {course.Course_Users} enrolled students, taught by{" "}
               <div className="text-bc">.</div>
+              <div onClick={()=>{ localStorage.setItem('course_instructor', course.Course_Instructor.Instructor_ID)}}>
               <Link href="instructor">
                 {/* // 23deli el link */}
                 <div className="text-violet-400">
@@ -157,7 +158,7 @@ const enroll = (isCorporate:string) => {
                 </div>
               </Link>
             </div>
-
+            </div>
             {/* /*div el country*/}
             <div className="flex flex-row gap-1 items-center">
               <BsGlobe2 />
--- acl-fogration-fe/components/molecules/UserCoursesCard.tsx
+++ acl-fogration-fe/components/molecules/UserCoursesCard.tsx
@@ -134,12 +134,16 @@ const enroll = (isCorporate:string) => {
               <div className="text-white flex flex-row">
                 Taught by
                 <div className="text-black3">.</div>
+                <div onClick={()=>{
+                  localStorage.setItem("course_instructor", course?.Course_Instructor?.Instructor_ID)
+                }} >
                 <Link href="instructor">
                   {/* // 23deli el link */}
                   <div className="text-violet-400">
                     {course?.Course_Instructor?.Instructor_FirstName}
                   </div>
                 </Link>
+                </div>
               </div>
               {/* /*div el country*/}
               <div className="flex flex-row gap-1 items-center">
