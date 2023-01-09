a852dfb324c53568db159a570bd91689c3fac0aa
salmasleem <salmasleem@outlook.com>
2023-01-09 18:30:37 +0100

    Admin Report

--- acl-fogration-fe/pages/admin/reports.tsx
+++ acl-fogration-fe/pages/admin/reports.tsx
@@ -11,29 +11,18 @@ const reports = () => {
 const [UserID,setUserID]=useState('')
 
 
-function getProfile(){
-  if(isCorp=="true"){
-  axios.post("http://localhost:8000/corporateProfile",{
-    User_ID:Number(localStorage.getItem("user_id"))
-  }
+function getProblem(){
+  
+  axios.get("http://localhost:8000/viewReportedProblems"
  ).then((response) => {
    setPerson(response.data)
-  //  console.log(response.data)
+   console.log(response.data)
  }).catch((error) => console.log(error))
-}
-else if(isCorp=="false"){
-  axios.post("http://localhost:8000/individualProfile",{
-    User_ID:Number(localStorage.getItem("user_id"))
-  }
- ).then((response) => {
-   setPerson(response.data)
- }).catch((error) => console.log(error))
-}
 }
 useEffect(() => {
   setCorp(localStorage.getItem("isCorp"))
   setUserID(localStorage.getItem("userID"))
-  getProfile();
+  getProblem();
 
 })
 useEffect(()=>{
@@ -42,7 +31,7 @@ useEffect(()=>{
     <div>
         <Layout>
         <div>
-            <ReportRequests courses={undefined}/>
+            <ReportRequests problems={person}/>
         </div>
       </Layout>
     </div>
